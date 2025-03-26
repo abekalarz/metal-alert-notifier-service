93b90a80405e53c3e2f8ac2d9187ae7a085b30ef
abekalarz <adam.bekalarz@gmail.com>
2025-03-26 10:24:57 +0100

    first commit

---
+++ src/main/java/com/example/metal_alert_notifier_service/MetalAlertNotifierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.metal_alert_notifier_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalAlertNotifierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetalAlertNotifierServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/metal_alert_notifier_service/MetalAlertNotifierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metal_alert_notifier_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalAlertNotifierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
